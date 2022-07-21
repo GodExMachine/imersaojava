fbd42bb90cca38cc611e07206ff0ed6843c7d176
Alex Jungklaus <88566095+GodExMachine@users.noreply.github.com>
2022-07-21 00:51:50 -0300

    Add files via upload

--- src/App.java
+++ src/App.java
@@ -1,4 +1,6 @@
+import java.io.InputStream;
 import java.net.URI;
+import java.net.URL;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
@@ -10,7 +12,7 @@ public class App {
     public static void main(String[] args) throws Exception {  
 
         // fazer um conexao HTTP e buscar os top 250 filmes
-        String url = "https://mocki.io/v1/9a7c1ca9-29b4-4eb3-8306-1adb9d159060";
+        String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java/api/TopMovies.json";
         URI endereco = URI.create(url);
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder(endereco).GET().build();
@@ -22,16 +24,33 @@ public class App {
         List<Map<String, String>> listaDeFilmes = parser.parse(body);
 
         // exibir e manipular os dados
-        for (Map<String,String> filme : listaDeFilmes) {
-            System.out.println("Título: "+filme.get("title")+"\u001b[0m");
-            System.out.println("Poster: "+filme.get("image"));
         
+        var geradora = new GeradorDeFiguras();
+
+        for (Map<String,String> filme : listaDeFilmes) {
+
+            String urlImagem = filme.get("image");
+            String titulo = filme.get("title");
+            
+            InputStream inputStream = new URL(urlImagem).openStream();
+            titulo = titulo.replace(":", "-");
+
+            String nomeArquivo ="saida/"+ titulo + ".png";
+
+           
+            geradora.cria(inputStream, nomeArquivo);
+
+            System.out.println(titulo);
+
+
+
+            
 
             String stars = "";
             int somaestrelas;
             
             for (somaestrelas = 0;somaestrelas < Double.valueOf(filme.get("imDbRating")).intValue();somaestrelas++) {
-                stars += "\u2B50";
+             stars += "\u2B50";
 
             }
             
@@ -46,3 +65,4 @@ public class App {
 
     }
 }
+
